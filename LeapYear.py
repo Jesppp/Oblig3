93ea08347c093780711c38050282485a4ca4fbe9
Jesper Andreas <111963917+Jesppp@users.noreply.github.com>
2022-10-27 12:53:33 +0200

    Update LeapYear.py

--- LeapYear.py
+++ LeapYear.py
@@ -1,7 +1,7 @@
 def isLeapYear(year: int) -> bool:
     if year % 4 == 0 and year % 100 != 0:
         return True
-    elif year % 400 == 1
+    elif year % 400 == 0:
         return True
     else:
         return False
